324c80d70ce9ae01e2db59999823941b8a3e6665
Rhenan Bartels <rhenan.bartels@gmail.com>
2018-05-23 16:16:48 -0300

    Reorganiza codigo

--- robobnmp/api.py
+++ robobnmp/api.py
@@ -1,36 +1,35 @@
 import json
-
 import requests
 
 
 REGISTROS = 50
+DADOS = {
+    'criterio': {
+        'orgaoJulgador': {
+            'uf': 'RJ',
+            'municipio': '',
+            'descricao': '',
+        },
+        'orgaoJTR': {},
+        'parte': {},
+    },
+    'paginador': {
+        'paginaAtual': None,
+        'registrosPorPagina': REGISTROS
+    },
+    'fonetica': 'true',
+    'ordenacao': {
+        'porNome': 'false',
+        'porData': 'false',
+    },
+}
 
 
 def _procura_mandados(pagina):
-    dados = {
-        'criterio': {
-            'orgaoJulgador': {
-                'uf': 'RJ',
-                'municipio': '',
-                'descricao': '',
-            },
-            'orgaoJTR': {},
-            'parte': {},
-        },
-        'paginador': {
-            'paginaAtual': pagina,
-            'registrosPorPagina': REGISTROS
-        },
-        'fonetica': 'true',
-        'ordenacao': {
-            'porNome': 'false',
-            'porData': 'false',
-        },
-    }
-
+    DADOS['paginador']['paginaAtual'] = pagina
     resp = requests.post(
         url='http://www.cnj.jus.br/bnmp/rest/pesquisar',
-        data=json.dumps(dados),
+        data=json.dumps(DADOS),
         headers={
             'Content-Type': 'application/json',
             'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
